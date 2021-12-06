248edfff2dbb8ef0b6d8f2734bfd10672cfbc6dd
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-12-05 17:17:57 -0800

    Format Config class

--- src/aco/Config.java
+++ src/aco/Config.java
@@ -1,12 +1,16 @@
 package aco;
 
 public class Config {
-    // power of the pheromone, helps decide how much emphasis on existing trails. Typically lesser than beta to protect against not discovering more paths
+    // Power of the pheromone, helps decide how much emphasis on existing trails.
+    // Typically, lesser than beta to protect against not discovering more paths.
     private static final double ALPHA = 1;
-    // power of distance, helps decide how much emphasis on distance
+
+    // Power of distance, helps decide how much emphasis on distance
     private static final double BETA = 2;
-    // rate of evaporation, to help reduce emphasis on existing paths
+
+    // Rate of evaporation, to help reduce emphasis on existing paths
     private static final double RHO = 0.5;
+
     // Just a constant
     private static final double Q3 = 100;
 
