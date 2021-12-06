9b05747fd6e53aaf60b70811bf0d2d4d8c1e53e1
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-12-05 17:55:34 -0800

    Reformat visualization code

--- viz/cvrp.py
+++ viz/cvrp.py
@@ -23,7 +23,7 @@ def plot_nodes(input_file):
         load_capacity = False
         for line in lines:
             line = line.strip()
-#             print(line)
+            #             print(line)
             if line == 'NODE_COORD_SECTION':
                 load_coordinates = True
                 continue
@@ -34,7 +34,7 @@ def plot_nodes(input_file):
             elif line == 'DEPOT_SECTION':
                 load_capacity = False
                 break
-            
+
             if load_coordinates:
                 i, x, y = [float(each) for each in line.split()]
                 # print(line, x, y)
@@ -47,8 +47,9 @@ def plot_nodes(input_file):
     # print(list(zip(x_coords, y_coords)))
     plt.scatter(x_coords, y_coords)
     for i in range(len(capacity)):
-        plt.annotate(i+1, (x_coords[i], y_coords[i]) , (x_coords[i], y_coords[i] - 3))
+        plt.annotate(i + 1, (x_coords[i], y_coords[i]), (x_coords[i], y_coords[i] - 3))
     return x_coords, y_coords
+
 
 def plot_routes(output_file, x_coords, y_coords):
     with open(output_file, 'r') as f:
@@ -60,12 +61,13 @@ def plot_routes(output_file, x_coords, y_coords):
             points = [int(each) for each in points]
             sol_x_coords = [x_coords[0]] + [x_coords[i] for i in points] + [x_coords[0]]
             sol_y_coords = [y_coords[0]] + [y_coords[i] for i in points] + [y_coords[0]]
-#             print(sol_x_coords)
-#             print(sol_y_coords)
-#             print('-'*10)
+            #             print(sol_x_coords)
+            #             print(sol_y_coords)
+            #             print('-'*10)
             plt.scatter(sol_x_coords, sol_y_coords)
             plt.plot(sol_x_coords, sol_y_coords)
-        
+
+
 def plot_route(output_file, x_coords, y_coords, route_num):
     with open(output_file, 'r') as f:
         lines = f.readlines()
@@ -76,9 +78,9 @@ def plot_route(output_file, x_coords, y_coords, route_num):
             points = [int(each) for each in points]
             sol_x_coords = [x_coords[0]] + [x_coords[i] for i in points] + [x_coords[0]]
             sol_y_coords = [y_coords[0]] + [y_coords[i] for i in points] + [y_coords[0]]
-#             print(sol_x_coords)
-#             print(sol_y_coords)
-#             print('-'*10)
+            #             print(sol_x_coords)
+            #             print(sol_y_coords)
+            #             print('-'*10)
             plt.scatter(sol_x_coords, sol_y_coords)
             plt.plot(sol_x_coords, sol_y_coords)
 
@@ -86,24 +88,19 @@ def plot_route(output_file, x_coords, y_coords, route_num):
 # In[57]:
 
 
-# folder = 'A'
-# name = 'A-n32-k5'
-# sol = 'Test-A-n32-k5'
+folder = 'A'
+name = 'A-n32-k5'
+sol = 'A-n32-k5'
 
 # folder = 'B'
 # name = 'B-n78-k10'
 
-folder = 'Golden'
-name = 'Golden_20'
-sol = 'Test-Golden_20'
+# folder = 'Golden'
+# name = 'Golden_20'
+# sol = 'Test-Golden_20'
 
 x_coords, y_coords = plot_nodes(f"{folder}/{name}.vrp")
-plot_route(f"{folder}/{sol}.sol", x_coords, y_coords, 2)
+plot_routes(f"{folder}/{sol}.sol", x_coords, y_coords)
 plt.show()
 
-
 # In[ ]:
-
-
-
-
